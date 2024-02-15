8b9780e5466677f4e5761adc54091d194367da48
Samuel Vítek <samuel.vitek@email.cz>
2024-02-15 17:19:48 +0200

    new: filling data manually (found out about API, possibly in future)

--- src/data/Types.ts
+++ src/data/Types.ts
@@ -3,10 +3,31 @@ export enum Skills {
     TS = 'Typescript',
     NEXT = 'Next.js',
     JAVA = 'Java',
+    SQL = 'SQL',
+    TEACHING = 'Teaching',
+    WEBDEV = 'Web Development',
+    TESTER = 'QA Testing',
 }
 
 export enum Tools {
     GA = 'Google Analytics',
     POSTMAN = 'Postman API',
     GITHUB = 'GitHub',
+    GIT = 'Git',
+    NOTION = 'Notion API',
+    SERP = 'Serp API',
+    MONGO = 'MongoDB',
+    PBI = 'PowerBI',
+    JIRA = 'Jira',
+    JENKINS = 'Jenkins',
+    SCRUM = 'Agile Dev - Scrum',
+}
+
+export type Data = {
+    title: string;
+    company: string;
+    location: string;
+    logo: string;
+    skills: Array<Skills | Tools>
+    date: string;
 }
