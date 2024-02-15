7359abd092734b2367871948d5312000d61c4196
Samuel Vítek <samuel.vitek@email.cz>
2024-02-15 14:42:32 +0200

    new: adding enums for skill tags and setting up first data to be imported dynamically

---
+++ src/data/Types.ts
@@ -0,0 +1,12 @@
+export enum Skills {
+    REACT = 'React.js',
+    TS = 'Typescript',
+    NEXT = 'Next.js',
+    JAVA = 'Java',
+}
+
+export enum Tools {
+    GA = 'Google Analytics',
+    POSTMAN = 'Postman API',
+    GITHUB = 'GitHub',
+}
---
+++ src/data/WorkExperience.ts
@@ -0,0 +1,32 @@
+import {Skills, Tools} from "./Types";
+
+export const WorkExperience = [
+    {
+        title: 'Full-Stack Developer',
+        company: 'WeddMate s.r.o.',
+        workplace: 'Remote (Czech Republic)',
+        logo: '/img/timeline/weddmate.png',
+        skills: [
+            Skills.REACT,
+            Skills.TS,
+            Skills.NEXT,
+            Tools.GITHUB,
+            Tools.POSTMAN,
+            Tools.GA
+        ]
+    },
+    {
+        title: 'Full-Stack Developer',
+        company: 'WeddMate s.r.o.',
+        workplace: 'Remote (Czech Republic)',
+        logo: '/img/timeline/weddmate.png',
+        skills: [
+            Skills.REACT,
+            Skills.TS,
+            Skills.NEXT,
+            Tools.GITHUB,
+            Tools.POSTMAN,
+            Tools.GA
+        ]
+    },
+]
