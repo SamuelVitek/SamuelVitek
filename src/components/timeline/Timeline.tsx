447cc4a2b7311232cf5e0e5bf3b7db386988b219
Samuel Vítek <samuel.vitek@email.cz>
2024-02-15 18:33:34 +0200

    new: work experience timeline components final

--- src/components/timeline/Timeline.tsx
+++ src/components/timeline/Timeline.tsx
@@ -1,75 +1,17 @@
 import React from 'react';
+import TimelineElement from "./TimelineElement";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { WorkExperience as data } from "../../data/WorkExperience";
+import { faGraduationCap, faPlay } from "@fortawesome/free-solid-svg-icons";
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faBriefcase, faGraduationCap, faLocationDot, faPlay} from "@fortawesome/free-solid-svg-icons";
-import TimelineElement from "./TimelineElement";
-import {Flex, Heading, Image, Tag, Text} from "@chakra-ui/react";
-
 
 const Timeline = () => {
-
     return (
         <VerticalTimeline>
-            <TimelineElement>
-                <Flex>
-                    <Flex w='80%' flexDirection='column'>
-                        <Heading style={{color: 'white'}}>Full-Stack Developer</Heading>
-                        <Text style={{margin: '0'}}>
-                            <FontAwesomeIcon icon={faBriefcase}/> WeddMate s.r.o.
-                        </Text>
-                        <Text style={{margin: '0 0 2.25em 0'}}>
-                            <FontAwesomeIcon icon={faLocationDot}/> Remote (Czech Republic)
-                        </Text>
-                    </Flex>
-                    <Flex>
-                        <Image src='/img/timeline/weddmatfe.png' alt='Logo' w='3em' h='3em' borderRadius='full' />
-                    </Flex>
-                </Flex>
-                <Tag me='2'>React.js</Tag>
-                <Tag me='2'>Typescript</Tag>
-                <Tag me='2'>Next.js</Tag>
-                <Tag me='2'>Postman</Tag>
-                <Tag me='2'>GitHub</Tag>
-                <Tag me='2'>Google Analytics</Tag>
-            </TimelineElement>
-            <VerticalTimelineElement
-                className="vertical-timeline-element--work"
-                date="2010 - 2011"
-                contentStyle={{ borderTop: 'rgb(33, 150, 243)', color: 'gray', boxShadow: 'none' }}
-                iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                icon={<FontAwesomeIcon icon={faBriefcase} />}
-            >
-                <h3 className="vertical-timeline-element-title">Art Director</h3>
-                <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
-                <p>
-                    Creative Direction, User Experience, Visual Design, SEO, Online Marketing
-                </p>
-            </VerticalTimelineElement>
-            <VerticalTimelineElement
-                className="vertical-timeline-element--work"
-                date="2008 - 2010"
-                iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                icon={<FontAwesomeIcon icon={faBriefcase} />}
-            >
-                <h3 className="vertical-timeline-element-title">Web Designer</h3>
-                <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
-                <p>
-                    User Experience, Visual Design
-                </p>
-            </VerticalTimelineElement>
-            <VerticalTimelineElement
-                className="vertical-timeline-element--work"
-                date="2006 - 2008"
-                iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                icon={<FontAwesomeIcon icon={faBriefcase} />}
-            >
-                <h3 className="vertical-timeline-element-title">Web Designer</h3>
-                <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
-                <p>
-                    User Experience, Visual Design
-                </p>
-            </VerticalTimelineElement>
+            {data.map((d, index) => (
+                <TimelineElement key={index} data={d} />
+            ))}
             <VerticalTimelineElement
                 className="vertical-timeline-element--education"
                 date="April 2013"
--- src/components/timeline/TimelineElement.tsx
+++ src/components/timeline/TimelineElement.tsx
@@ -1,13 +1,15 @@
-import React, {ReactNode} from 'react';
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faBriefcase} from "@fortawesome/free-solid-svg-icons";
-import {VerticalTimelineElement} from "react-vertical-timeline-component";
+import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Flex, Heading, Image, Tag, Text } from "@chakra-ui/react";
+import { faBriefcase, faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import { Data } from "../../data/Types";
+import { VerticalTimelineElement } from "react-vertical-timeline-component";
 
-type TimeElementProps = {
-    children: ReactNode;
+interface PropsType {
+    data: Data;
 }
 
-const TimelineElement = (props: TimeElementProps) => {
+const TimelineElement = ({data}: PropsType) => {
     return (
         <VerticalTimelineElement
             className="vertical-timeline-element--work"
@@ -19,12 +21,28 @@ const TimelineElement = (props: TimeElementProps) => {
                 textAlign: 'left',
                 paddingBottom: '0.75em'
             }}
-            contentArrowStyle={{ borderRight: '7px solid  rgb(2, 41, 79)' }}
-            date="2011 - present"
-            iconStyle={{ background: 'rgb(2, 41, 79)', color: '#fff' }}
-            icon={<FontAwesomeIcon icon={faBriefcase} />}
+            contentArrowStyle={{borderRight: '7px solid  rgb(2, 41, 79)'}}
+            date={data.date}
+            iconStyle={{background: 'rgb(2, 41, 79)', color: '#fff'}}
+            icon={<FontAwesomeIcon icon={faBriefcase}/>}
         >
-            {props.children}
+            <Flex>
+                <Flex w='80%' flexDirection='column'>
+                    <Heading style={{color: 'white'}}>{data.title}</Heading>
+                    <Text style={{margin: '0'}}>
+                        <FontAwesomeIcon icon={faBriefcase}/> {data.company}
+                    </Text>
+                    <Text style={{margin: '0 0 2.25em 0'}}>
+                        <FontAwesomeIcon icon={faLocationDot}/> {data.location}
+                    </Text>
+                </Flex>
+                <Flex>
+                    <Image src={data.logo} fit='cover' alt='Logo' w='3.125em' h='3.125em' borderRadius='full'/>
+                </Flex>
+            </Flex>
+            {data.skills.map((skill, index) => (
+                <Tag me='2' key={index + '_' + skill}>{skill}</Tag>
+            ))}
         </VerticalTimelineElement>
     );
 };
