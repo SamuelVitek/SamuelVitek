9a989ac4e3efb12c96f837cf5e27495f8bdf0a3d
Samuel Vítek <samuel.vitek@email.cz>
2024-02-19 03:11:44 +0200

    fix: switching router because of GitHub pages support (test)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 import React from 'react';
 import { ChakraProvider, Flex, theme } from "@chakra-ui/react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import Nav from "./components/navigation/Nav";
 import About from "./pages/About";
 import Home from "./pages/Home";
@@ -10,7 +10,7 @@ import Work from "./pages/Work";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
     { path: '/SamuelVitek/', element: <Home/> },
     { path: '/SamuelVitek/about', element: <About/> },
     { path: '/SamuelVitek/work', element: <Work/> },
