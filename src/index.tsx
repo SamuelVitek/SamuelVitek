b19ec5185bc525ce74759750581acdc50c6ff240
Samuel Vítek <samuel.vitek@email.cz>
2024-02-19 04:36:39 +0200

    fix: switching router because of GitHub pages support (test)

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
     <React.StrictMode>
-        <HashRouter basename={`${process.env.PUBLIC_URL}/`}>
+        <HashRouter>
             <App />
         </HashRouter>
     </React.StrictMode>
