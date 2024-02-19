d11e3474f4a9927187ab27bf91aaefcbcce71f43
Samuel Vítek <samuel.vitek@email.cz>
2024-02-19 03:28:06 +0200

    fix: switching router because of GitHub pages support (test)

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,12 @@ import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
 
 const router = createBrowserRouter([
-    { path: `${process.env.PUBLIC_URL}/`, element: <Home/> },
-    { path: `${process.env.PUBLIC_URL}/about`, element: <About/> },
-    { path: `${process.env.PUBLIC_URL}/work`, element: <Work/> },
-    { path: `${process.env.PUBLIC_URL}/education`, element: <Edu/> },
-    { path: `${process.env.PUBLIC_URL}/projects`, element: <Projects/> },
-    { path: `${process.env.PUBLIC_URL}/contact`, element: <Contact/> }
+    { path: `/${process.env.PUBLIC_URL}`, element: <Home/> },
+    { path: `/${process.env.PUBLIC_URL}/about`, element: <About/> },
+    { path: `/${process.env.PUBLIC_URL}/work`, element: <Work/> },
+    { path: `/${process.env.PUBLIC_URL}/education`, element: <Edu/> },
+    { path: `/${process.env.PUBLIC_URL}/projects`, element: <Projects/> },
+    { path: `/${process.env.PUBLIC_URL}/contact`, element: <Contact/> }
 ])
 
 function App() {
