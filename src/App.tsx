85078aab14174e0325fee57589c19aa34afb6cef
Samuel Vítek <samuel.vitek@email.cz>
2024-02-19 04:21:34 +0200

    fix: switching router because of GitHub pages support (test)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 import React from 'react';
 import { ChakraProvider, Flex, theme } from "@chakra-ui/react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {createBrowserRouter, Route, RouterProvider, Routes} from "react-router-dom";
 import Nav from "./components/navigation/Nav";
 import About from "./pages/About";
 import Home from "./pages/Home";
@@ -10,20 +10,24 @@ import Work from "./pages/Work";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
 
-const router = createBrowserRouter([
-    { path: '/SamuelVitek/', element: <Home/> },
-    { path: '/SamuelVitek/about', element: <About/> },
-    { path: '/SamuelVitek/work', element: <Work/> },
-    { path: '/SamuelVitek/education', element: <Edu/> },
-    { path: '/SamuelVitek/projects', element: <Projects/> },
-    { path: '/SamuelVitek/contact', element: <Contact/> }
-])
+// const router = createBrowserRouter([
+//     { path: '/SamuelVitek/', element: <Home/> },
+//     { path: '/SamuelVitek/about', element: <About/> },
+//     { path: '/SamuelVitek/work', element: <Work/> },
+//     { path: '/SamuelVitek/education', element: <Edu/> },
+//     { path: '/SamuelVitek/projects', element: <Projects/> },
+//     { path: '/SamuelVitek/contact', element: <Contact/> }
+// ])
 
 function App() {
     return (
         <div className="App">
             <ChakraProvider theme={theme}>
                     <Nav/>
+                    <Routes>
+                        <Route path="/" element={<Home />}/>
+                        <Route path="/about" element={<About />}/>
+                    </Routes>
                     <div className="App-body">
                         <Flex w='75%'>
                             <RouterProvider router={router} />
