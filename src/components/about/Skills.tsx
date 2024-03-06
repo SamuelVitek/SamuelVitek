3ed83edcdc2f08196ad7ef8c33d1d4568ee348b1
Samuel Vítek <samuel.vitek@email.cz>
2024-03-06 17:05:51 +0200

    new: adding tech stack to the skills and about me

--- src/components/about/Skills.tsx
+++ src/components/about/Skills.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
-import { Flex, SimpleGrid } from '@chakra-ui/react';
+import { Text, Flex, GridItem, Heading, SimpleGrid } from '@chakra-ui/react';
 import { langs } from '../../data/Others';
 import Language from './Language';
+import TechStack from "./TechStack";
 
 const Skills: React.FC = () => {
     return (
-        <Flex>
-            <Flex>
-                {/*TODO tech stack - Web Dev - Backend - Data Science*/}
+        <Flex justifyContent='space-between' mb='10vh'>
+            <Flex flexDirection='column'>
+                <Heading>My Tech-Stack</Heading>
+                <Text color='whiteAlpha.600' fontSize='lg' mb='10'>
+                    I can use these following technologies
+                </Text>
+                <TechStack/>
             </Flex>
             <SimpleGrid columns={3} spacingX='20' spacingY='10'>
+                <GridItem colSpan={3}>
+                    <Heading>Languages I speak</Heading>
+                    <Text color='whiteAlpha.600' fontSize='lg'>
+                        At least try to
+                    </Text>
+                </GridItem>
                 {langs.map(lang => (
                     <Language key={lang.lang} {...lang} />
                 ))}
